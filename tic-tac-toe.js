352ef39fe0193ca3dac5a599142a2d0c7ca3106f
Jahmoe Brown <jahmoenrown@gmail.com>
2023-10-22 21:25:24 -0500

    Updated Checkwin function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,13 @@
-window.onload= function()
-{
-    const statEl = document.getElementById("status");
+window.onload = function () {
+    const statusEl = document.getElementById("status");
     const boardEl = document.getElementById("board");
-    const buttonstrt = document.getElementsByClassName("btn")[0];
+    const buttonEl = document.getElementsByClassName("btn")[0];
 
-    const gmesqr = boardEl.querySelectorAll('div');
-    let player = 'X';
-    let sqr_pos = ['', '', '', '', '', '', '', '', ''];
+    const gameSquares = boardEl.querySelectorAll('div');
+    let currentPlayer = 'X';
+    let squarePositions = ['', '', '', '', '', '', '', '', ''];
 
-    const winningConditions = 
-    [
+    const winning = [
         [0, 4, 8],
         [3, 4, 5],
         [2, 4, 6],
@@ -20,69 +18,56 @@ window.onload= function()
         [6, 7, 8]
     ];
 
-
-    for(let j=0; j<=8; j++)
-    {
-        gmesqr[j].setAttribute("class","square");
+    for (let j = 0; j <= 8; j++) {
+        gameSquares[j].setAttribute("class", "square");
     }
 
-    gmesqr.forEach((seg, position) => 
-    {
-        seg.addEventListener('click', () => userAction(seg, position));
-        seg.addEventListener('mouseover', function()
-        {
-        seg.classList.add('hover');
-       // seg.removeEventListener('click', () => userAction(seg, position))
+    gameSquares.forEach((square, position) => {
+        square.addEventListener('click', () => handleUserAction(square, position));
+        square.addEventListener('mouseover', function () {
+            square.classList.add('hover');
         });
-        seg.addEventListener('mouseout', function(){
-            seg.classList.remove('hover');
+        square.addEventListener('mouseout', function () {
+            square.classList.remove('hover');
         });
     });
-    function checkWin()
-    {
-        for(let i=0; i<=7; i++){
-            const win = winningConditions[i];
- 
-            const pos1= sqr_pos[win[0]];
-            const pos2 = sqr_pos[win[1]];
-            const pos3 = sqr_pos[win[2]];
-            if (pos1 === ''|| pos2 === ''|| pos3===''){
+
+    function checkWin() {
+        for (let i = 0; i <= 7; i++) {
+            const win = winning[i];
+
+            const pos1 = squarePositions[win[0]];
+            const pos2 = squarePositions[win[1]];
+            const pos3 = squarePositions[win[2]];
+            if (pos1 === '' || pos2 === '' || pos3 === '') {
                 continue;
-            }  
-            if (pos1 === pos2 && pos2 === pos3)
-            {
-                statEl.innerHTML= 'Congratulations!' +  pos1 +' is the winner';
-                statEl.classList.add('you-won');
+            }
+            if (pos1 === pos2 && pos2 === pos3) {
+                statusElement.innerHTML = 'Congratulations! ' + pos1 + ' is the winner';
+                statusElement.classList.add('you-won');
                 break;
-            }     
+            }
         }
     }
-    const userAction= (seg,position) =>
-    {
-        console.log(seg.innerText)
 
-        if(seg.innerText !== 'X' || seg.innerText !== 'O')
-        {
-            seg.innerText= player;
-            seg.classList.add(player);
-            sqr_pos[position]=player;
-            console.log(sqr_pos);
+    const handleUserAction = (square, position) => {
+        if (square.innerText !== 'X' && square.innerText !== 'O') {
+            square.innerText = currentPlayer;
+            square.classList.add(currentPlayer);
+            squarePositions[position] = currentPlayer;
             checkWin();
-            if (player === 'X' ? player ='O' : player ='X');
+            currentPlayer = (currentPlayer === 'X' ? 'O' : 'X');
         }
-
     }
 
-    buttonstrt.addEventListener('click', ()=>
-    {
-        sqr_pos=['', '', '', '', '', '', '', '', ''];
-        statEl.innerHTML= 'Move your mouse over a square and click to play an X or an O.'
-        statEl.classList.remove('you-won');
-        gmesqr.forEach(seg =>
-            {
-            seg.innerText ='';
-            seg.classList.remove('X');
-            seg.classList.remove('O');
+    buttonEl.addEventListener('click', () => {
+        squarePositions = ['', '', '', '', '', '', '', '', ''];
+        statusEl.innerHTML = 'Move your mouse over a square and click to play an X or an O.';
+        statusEl.classList.remove('you-won');
+        gameSquares.forEach(square => {
+            square.innerText = '';
+            square.classList.remove('X');
+            square.classList.remove('O');
         });
     });
 }
