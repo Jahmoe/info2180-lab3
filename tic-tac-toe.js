30c2fd7e7c73532da885821f8a715a4e4880834a
Jahmoe Brown <jahmoenrown@gmail.com>
2023-10-22 21:03:31 -0500

    Added exercise 4-6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,23 +1,23 @@
 window.onload= function()
 {
-    const status = document.getElementById("status");
-    const board = document.getElementById("board");
-    const button = document.getElementsByClassName("btn")[0];
+    const statEl = document.getElementById("status");
+    const boardEl = document.getElementById("board");
+    const buttonstrt = document.getElementsByClassName("btn")[0];
 
-    const gmesqr = board.querySelectorAll('div');
+    const gmesqr = boardEl.querySelectorAll('div');
     let player = 'X';
     let sqr_pos = ['', '', '', '', '', '', '', '', ''];
 
     const winningConditions = 
     [
-        [0, 1, 2],
+        [0, 4, 8],
         [3, 4, 5],
-        [6, 7, 8],
+        [2, 4, 6],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2],
+        [6, 7, 8]
     ];
 
 
@@ -38,3 +38,51 @@ window.onload= function()
             seg.classList.remove('hover');
         });
     });
+    function checkWin()
+    {
+        for(let i=0; i<=7; i++){
+            const win = winningConditions[i];
+ 
+            const pos1= sqr_pos[win[0]];
+            const pos2 = sqr_pos[win[1]];
+            const pos3 = sqr_pos[win[2]];
+            if (pos1 === ''|| pos2 === ''|| pos3===''){
+                continue;
+            }  
+            if (pos1 === pos2 && pos2 === pos3)
+            {
+                statEl.innerHTML= 'Congratulations!' +  pos1 +' is the winner';
+                statEl.classList.add('you-won');
+                break;
+            }     
+        }
+    }
+    const userAction= (seg,position) =>
+    {
+        console.log(seg.innerText)
+
+        if(seg.innerText !== 'X' || seg.innerText !== 'O')
+        {
+            seg.innerText= player;
+            seg.classList.add(player);
+            sqr_pos[position]=player;
+            console.log(sqr_pos);
+            checkWin();
+            if (player === 'X' ? player ='O' : player ='X');
+        }
+
+    }
+
+    buttonstrt.addEventListener('click', ()=>
+    {
+        sqr_pos=['', '', '', '', '', '', '', '', ''];
+        statEl.innerHTML= 'Move your mouse over a square and click to play an X or an O.'
+        statEl.classList.remove('you-won');
+        gmesqr.forEach(seg =>
+            {
+            seg.innerText ='';
+            seg.classList.remove('X');
+            seg.classList.remove('O');
+        });
+    });
+}
